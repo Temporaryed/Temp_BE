dae74989b01d3637e27cf1e8ddedbd76e37c5cac
nn98 <jkllhgb@gmail.com>
2023-04-22 08:34:35 +0900

    Feat: UserInfo input with Get

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
     // res.render('index', { title: 'Express' });
     res.render('front', {title: 'sdzfzsdf'});
+    var userInfo = [req.query.ID, req.query.PW]
+    console.log(userInfo)
 });
 
 module.exports = router;
