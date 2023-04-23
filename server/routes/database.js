54ea8f16614a75c6ff78a72e38232abd1df8122b
nn98 <jkllhgb@gmail.com>
2023-04-23 18:02:38 +0900

    Feat: Mysql router

---
+++ server/routes/database.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var connection = mysql.createPool({
+    host: 'localhost',
+    user: 'Project',
+    password: 'testing00',
+    database: 'swp',
+    multipleStatements: true,
+    charset: 'utf8mb4',
+    connectionLimit: 30,
+});
+
+connection.getConnection((err) => {
+    if (err) {
+        console.log('Error:', err)
+    }
+    console.log('connecting');
+});
