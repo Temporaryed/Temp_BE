3f35e51b0d2fada5b8b7edad46d9ad8aec0fa5d2
nn98 <jkllhgb@gmail.com>
2023-04-24 23:45:13 +0900

    Feat: Apply string template

--- server/routes/front.js
+++ server/routes/front.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var context = 'Front'
+var paths = {
+    defaultPath: '/',
+}
+var logMsg = {
+    called: 'is called',
+    callBack: 'is callback'
+}
+
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-    // res.render('index', { title: 'Express' });
-    res.render('front', {title: 'sdzfzsdf'});
+
+
+    var path = paths.defaultPath
+    console.log(`\t${context}:\t${path} is ${logMsg.called}`)
+
+    res.render('front', {title: 'sdzfzsdf', currentURL: path + context});
+
     var userInfo = [req.query.ID, req.query.PW]
-    console.log(userInfo)
+    console.log('userInfo:', userInfo)
+
+    console.log(`\t${context}:\t${path} is ${logMsg.callBack}`)
 });
 
 module.exports = router;
